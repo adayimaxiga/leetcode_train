3141b6cbec79879674dde49f995a430af909e3d9
reed.lu <adayimaxiga@hotmail.com>
2020-10-21 16:06:09 +0800

    add 11#

--- 11.盛最多水的容器.cpp
+++ 11.盛最多水的容器.cpp
@@ -8,7 +8,27 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-
+        int max = 0;
+        for(int i = 0; i < height.size()-1;i++)
+        {
+            for(int j = i+1; j < height.size();j++)
+            {
+                int hh = 0;
+                if(height[i] > height[j])
+                {
+                    hh = height[j];
+                }else
+                {
+                    hh = height[i];
+                }
+                hh *= (j-i);
+                if(max<hh)
+                {
+                    max = hh;
+                }
+            }
+        }
+        return max;
     }
 };
 // @lc code=end
